fa324414485f5d8e062ae59e379367f5d646111b
Tommy Bedford <bedfordbox@gmail.com>
2020-08-13 19:34:44 +0100

    added maps module using mapbox.

--- HomeDev_Portfolio_Web/HomeDev_Portfolio_Web/Controllers/HomeController.cs
+++ HomeDev_Portfolio_Web/HomeDev_Portfolio_Web/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace HomeDev_Portfolio_Web.Controllers
             return View();
         }
 
+        public IActionResult Maps()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
